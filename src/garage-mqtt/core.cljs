8f745c62185930c06a0c9c6ad8d2cf982f43c2b1
Stian Eikeland <stian@eikeland.se>
2019-01-19 01:13:42 +0100

    Working MQTT communicaton

--- src/garage-mqtt/core.cljs
+++
@@ -1,4 +0,0 @@
-(ns garage-mqtt.core)
-
-(prn "Hello world")
-
---
+++ src/garage_mqtt/core.cljs
@@ -0,0 +1,52 @@
+(ns garage-mqtt.core
+  (:require [mqtt]))
+
+(def mqtt-uri           "mqtt://test.mosquitto.org")
+(def topic-availability "garage/door/availability")
+(def topic-set          "garage/door/set")
+(def topic-state        "garage/door/state")
+
+(def options {:keepalive (* 60 5)
+              :will {:topic topic-availability
+                     :payload "offline"
+                     :qos 0
+                     :retain true}})
+
+(defn log [what]
+  (let [ts (.toISOString (new js/Date (js/Date.now)))]
+    (println (str "[" ts "] " what))))
+
+(def client (mqtt/connect mqtt-uri (clj->js options)))
+
+(defn toggle-opener! []
+  (log "Toggling garage opener.."))
+
+(defn current-state []
+  ::closed)
+
+(defn publish-state []
+  (let [state (name (current-state))
+        opts  (clj->js {:retain true})]
+    (.publish client topic-state state opts)))
+
+(defn dispatch-command [cmd]
+  (case [(current-state) cmd]
+    [::closed "OPEN"]  (toggle-opener!)
+    [::open   "CLOSE"] (toggle-opener!)))
+
+(defn on-connect []
+  (log "Connected to MQTT!")
+  (.subscribe client topic-set)
+  (.publish client topic-availability "online")
+  (publish-state))
+
+(defn on-message [topic message]
+  (let [msg-str (.toString message)]
+    (log (str "Got command: " msg-str))
+    (when (= topic topic-set)
+      (dispatch-command msg-str))))
+
+(.on client "connect" on-connect)
+(.on client "message" on-message)
+
+(log "Connecting to MQTT..")
